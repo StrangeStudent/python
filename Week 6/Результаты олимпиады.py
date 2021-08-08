924a71406483c84b10dea64e918d20184884d46b
StrangeStudent <87879662+StrangeStudent@users.noreply.github.com>
2021-08-08 17:13:43 +0300

    Delete Week 6 directory

--- Week 6/Результаты олимпиады.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-participants = []
-for i in range(n):
-    ktemp = input().split()
-    k = ((ktemp[0]), int(ktemp[1]))
-    participants.append(k)
-sorted_list = sorted(participants, key=lambda x: x[1])
-for i in reversed(sorted_list):
-    print(i[0])
